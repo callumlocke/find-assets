c119d5bb9fe1315653345bd10ce5ba9841ddd444
Callum Locke <callum.locke@gmail.com>
2014-08-04 20:37:11 +0100

    don't group references with non-whitespace text between
    fixes #3

--- html.js
+++ html.js
@@ -15,12 +15,28 @@ var isLocalStylesheet = function (attrs) {
   return attrs.rel === 'stylesheet' && isLocalURL(attrs.href);
 };
 
-var isGroupableTag = function (tagName, attrs) {
+var isTagGroupable = function (tagName, attrs) {
   return (
     (tagName === 'script' && isLocalScript(attrs)) ||
     (tagName === 'link' && isLocalStylesheet(attrs))
   );
 };
+
+var isWhitespace = function (str) {
+  var c, i = str.length;
+  while (i--) {
+    c = str.charCodeAt(i);
+    switch (c) {
+      case 0x0009: case 0x000A: case 0x000B: case 0x000C: case 0x000D: case 0x0020:
+      case 0x0085: case 0x00A0: case 0x1680: case 0x180E: case 0x2000: case 0x2001:
+      case 0x2002: case 0x2003: case 0x2004: case 0x2005: case 0x2006: case 0x2007:
+      case 0x2008: case 0x2009: case 0x200A: case 0x2028: case 0x2029: case 0x202F:
+      case 0x205F: case 0x3000: continue;
+    }
+    return false;
+  }
+  return true;
+}
 
 var urlAttribute = {
   script: 'src',
@@ -43,7 +59,7 @@ module.exports = function (html, limit) {
   var groups = [];
   var currentReference, currentGroup, lastElementEndIndex;
 
-  var completeCurrentReference = function () {
+  var completeReference = function () {
     currentReference.end = lastElementEndIndex;
     currentReference.string = html.substring(currentReference.start, currentReference.end);
     currentGroup.references.push(currentReference);
@@ -51,24 +67,25 @@ module.exports = function (html, limit) {
     currentReference = null;
   };
 
-  var completeCurrentGroup = function () {
+  var finaliseGroup = function () {
     groups.push(currentGroup);
     currentGroup = null;
   };
 
   var parser = new htmlparser.Parser({
     onopentag: function (tagName, attrs) {
-      if (currentReference) completeCurrentReference();
+      if (currentReference) completeReference();
 
-      if (isGroupableTag(tagName, attrs)) {
+      if (isTagGroupable(tagName, attrs)) {
         var type = tagName === 'script' ? 'script' : 'stylesheet';
 
+        // finalise the current group if necessary
         if (currentGroup && (
           currentGroup.type !== type ||
           currentGroup.limitToOne ||
           currentGroup.references.length >= limit
         )) {
-          completeCurrentGroup();
+          finaliseGroup();
         }
 
         // see if this one has a hash or query string (which means it can't be grouped)
@@ -77,11 +94,10 @@ module.exports = function (html, limit) {
         if (url.indexOf('#') !== -1 || url.indexOf('?') !== -1)
           limitToOne = true;
 
-        // if this next group is limitToOne, we must complete the last one too
-        if (currentGroup && limitToOne)
-          completeCurrentGroup();
+        // if we need to limit this next group to one, we must finalise the last one too
+        if (limitToOne && currentGroup) finaliseGroup();
 
-        // start the next group
+        // start the next group if necessary
         if (!currentGroup) {
           currentGroup = {
             type: type,
@@ -90,7 +106,7 @@ module.exports = function (html, limit) {
           };
         }
 
-        // start off the reference object (will be finished off after)
+        // start off the reference object (will be finished off when we reach the close tag)
         currentReference = {
           type: tagName === 'script' ? 'script' : 'stylesheet',
           url: url,
@@ -100,8 +116,10 @@ module.exports = function (html, limit) {
       else {
         // this tag isn't a concatenatable type.
 
-        if (currentGroup) completeCurrentGroup();
+        // complete the current group if there is one
+        if (currentGroup) finaliseGroup();
 
+        // add a [non-groupable] reference for this one
         switch (tagName) {
         case 'img':
         case 'source':
@@ -123,13 +141,21 @@ module.exports = function (html, limit) {
       }
     },
 
+    ontext: function (text) {
+      // if there's any non-whitespace in this text node, finalise any current groups/refs
+      if (!isWhitespace(text)) {
+        if (currentReference) completeReference();
+        if (currentGroup) finaliseGroup();
+      }
+    },
+
     onclosetag: function (tagName) {
       lastElementEndIndex = parser.endIndex + 1;
     },
 
     onend: function () {
-      if (currentReference) completeCurrentReference();
-      if (currentGroup) completeCurrentGroup();
+      if (currentReference) completeReference();
+      if (currentGroup) finaliseGroup();
     }
   });
 
--- test/test.coffee
+++ test/test.coffee
@@ -2,15 +2,16 @@ findAssets = require '..'
 path = require 'path'
 fs = require 'fs'
 _ = require 'lodash'
-CSON = require 'cson'
 {expect} = require 'chai'
 
 describe '.html()', ->
-  expected = CSON.parseFileSync(path.resolve(__dirname, 'expected', 'html.cson'))
+  expected = require(path.resolve(__dirname, 'expected', 'html.json'));
   html = fs.readFileSync(path.resolve(__dirname, 'fixtures', 'example.html')).toString()
+
 
   it 'finds references and groups them', ->
     references = findAssets.html(html)
+    fs.writeFileSync(path.resolve(__dirname, 'OUTPUT.json'), JSON.stringify(references, null, 2))
     expect(references).to.deep.equal(expected)
 
   it 'can disable grouping', ->
